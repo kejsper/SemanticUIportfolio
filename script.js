aa55e3efb819134dd03c857bf3bac97a6d13520e
kejsper\Programowanie <kejsper@gmail.com>
2017-06-18 12:28:46 +0200

    Semantic part 2 done

--- script.js
+++ script.js
@@ -8,4 +8,46 @@ $(document).ready(function() {
   });
 
 
+  $('.ui.form').form({
+    on: 'submit',
+    fields: {
+      firstname: {
+        identifier: 'firstname',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please fill First Name input'
+          }
+        ]
+      },
+      lastname: {
+        identifier: 'lastname',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please fill Last Name input'
+          }
+        ]
+      },
+      email: {
+        identifier: 'email',
+        rules: [
+          {
+            type: 'email',
+            prompt: 'Please enter valid email address'
+          }
+        ]
+      },
+      message: {
+        identifier: 'message',
+        rules: [
+          {
+            type: 'minLength[20]',
+            prompt: 'Please enter minimum of 20 characters'
+          }
+        ]
+      },
+    }
+  });
+
 });
